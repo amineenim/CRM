b12e82f78e2f4b054bd90d87e221938f3af3de97
amineenim <aminemaourid1995@gmail.com>
2023-06-01 02:46:03 +0200

    adding the base template, login page and the login functionnality with a message in the success or failure

--- djangoCRM/website/views.py
+++ djangoCRM/website/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
+# the home page view 
+def home(request) : 
+    # check if the form is submitted
+    if request.method == "POST" :
+        # get form data
+        username = request.POST['username']
+        password = request.POST['password']
+        # authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None :
+            login(request, user)
+            messages.success(request,"Welcome back !")
+            return redirect('home')
+        else :
+            messages.error(request, "something went wrong, please verify your credentials !")
+            return redirect('home')
+    return render(request, 'home.html', {})
+
+# function that handles login in a user
+def login_user(request) :
+    pass
+
+# function that handles logging out a user
+def logout_user(request) : 
+    pass 
